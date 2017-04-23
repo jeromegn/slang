f327853a8b6457e92506181020622efe93b5c8b1
Isaac Sloan <isaac@isaacsloan.com>
2017-04-23 07:54:41 -0600

    fixed small bug where css and javascript blocks didn't work if they were indented past the first line (#19)

--- src/slang/nodes/text.cr
+++ src/slang/nodes/text.cr
@@ -1,6 +1,10 @@
 module Slang
   module Nodes
     class Text < Node
+      def allow_children_to_escape?
+        parent.not_nil!.allow_children_to_escape?
+      end
+
       def to_s(str, buffer_name)
         str << "#{buffer_name} << \"\n\"\n" unless str.empty? || inline
         str << "#{buffer_name} << \"#{indentation}\"\n" if indent?
@@ -11,6 +15,11 @@ module Slang
           str << "(#{value})"
         end
         str << ".to_s(#{buffer_name})\n"
+        if children?
+          nodes.each do |node|
+            node.to_s(str, buffer_name)
+          end
+        end
       end
     end
   end

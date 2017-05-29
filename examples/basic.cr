cb1f75b5ca08cba85c14cf8072faba419aaf6b54
Jerome Gravel-Niquet <jeromegn@gmail.com>
2017-05-29 16:11:44 -0400

    fix embed_slang -> Slang.embed

--- examples/basic.cr
+++ examples/basic.cr
@@ -4,7 +4,7 @@ some_var = "hello"
 strings = ["ah", "oh"]
 
 # res = String.build do |__slang__|
-#   embed_slang("basic.slang", "__slang__")
+#   Slang.embed("basic.slang", "__slang__")
 # end
 
 res = Slang.process_file("#{__DIR__}/../spec/fixtures/basic.slang")
